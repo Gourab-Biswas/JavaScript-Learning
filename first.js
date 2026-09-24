2208f79ee47ba774e9d27cd64b41f9559ac1cc44
Gourab-Biswas <gourab1331@outlook.com>
2026-09-25 02:05:18 +0530

    first day

---
+++ first.js
@@ -0,0 +1,40 @@
+// console.log("Hello, World!");
+// let a = 5;
+// console.log(`the value is ${a}`)
+// console.log(typeof(a))
+// console.log(typeof a)
+
+// let str = '10';
+// // let result = +str;
+// let result = Number(str);
+// console.log( typeof result)
+
+// let a = 5;
+// let result = String(a);
+// console.log( typeof result)
+
+
+// let a = "42.5"
+// let result = parseFloat(a);
+// console.log(result)
+// console.log(typeof result)
+
+//?type coercion
+// console.log( typeof ("5" + 10))
+// console.log( ("5" - 3))
+// console.log(typeof ("5" - 3))
+
+// let str = "3"
+// let result = Number(str)+2;
+// console.log(result)
+// console.log(typeof result)
+
+// console.log(true+true+true)
+// console.log(true-true)
+// console.log(true+false)
+// console.log(false+false)
+// console.log(false-true)
+
+
+
+// console.log((0.1+0.2).toFixed(2))
