5af0d0ea57d3544c6e857853b6437889a898de78
Gourab-Biswas <gourab1331@outlook.com>
2026-09-25 11:49:46 +0530

    day2 file added

---
+++ day2.js
@@ -0,0 +1,111 @@
+//! OPERATORS
+//? ARITHMETIC
+// console.log(5+3)
+// console.log(5-3)
+// console.log(6*3)
+// console.log(6/3)
+// console.log(6%4)
+
+// //? assignment
+// let number = 5;
+
+// //? String
+// let str = "apple";
+// let str2 = "banana";
+// let str3 = str+str2;
+// console.log(str3)
+
+// //? comparison operator
+// console.log(5==5)
+// console.log("5"==5)
+// console.log("5"===5)
+// console.log('5'!==5)
+// console.log(5>3)
+// console.log(5<3)
+// console.log(5>5)
+// console.log(5>=5)
+
+// //? Logical
+// let a = -90;
+// // if( a>=5 && a<=10){
+// //     console.log("hii")
+// // }
+// if( a>=5 || a<=10){
+//     console.log("hii")
+// }
+
+// //? unary
+
+// let a = 5;
+
+// console.log(-"3")
+// // console.log(a)
+
+
+// //? conditional [ternary]
+// let a = 5;
+// let result = a>=3?"yes":"no";
+// console.log(result);
+
+
+//? IF
+// let a =1;
+// if(a>5){
+//     console.log("yes");
+    
+// }else{
+//     console.log("no");
+    
+// }
+
+// //? switch
+
+// let a = "Y";
+// switch(a){
+//     case 1:
+//         console.log("this is 1");
+//         break
+//     case 2: 
+//         console.log("this is 2");
+//         break;
+//     case "Y":
+//         console.log("this is 3");
+//         break
+//     case "y": 
+//         console.log("this is 4");
+//         break;
+//     default:
+//         console.log("errorr>>>>");
+        
+// }
+
+// //? template literals
+// let age = 22;
+// console.log("I am "+age+" years old");
+// console.log("I am ",age," years old");
+// console.log(`I am ${age} year old boy`)
+
+//? while loop
+// let i = 1;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// //? do while
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=10);
+
+//? For loop
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// let str = ""
+// for(let i=0;i<=10;i++){
+//     str+=`${i} `
+// }
+// console.log(str)
